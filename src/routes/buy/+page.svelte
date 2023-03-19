d1b6f75cf37ce84a08fc75ce95391c4b5e361277
Yuvateja Reddy <yuvatejareddy003@gmail.com>
2023-03-19 17:31:23 +0530

    Style fix

--- src/routes/buy/+page.svelte
+++ src/routes/buy/+page.svelte
@@ -1,32 +1,32 @@
 <script>
-	let title = 'Kryptonit | Buy';
-	import transakSDK from '@transak/transak-sdk';
-	import { onMount } from 'svelte';
-	import { userAddress } from '../../store';
-	import Sidebar from '../../components/Sidebar.svelte';
-	import Topbar from '../../components/Topbar.svelte';
-	import Footer from '../../components/Footer.svelte';
+	let title = "Kryptonit | Buy";
+	import transakSDK from "@transak/transak-sdk";
+	import { onMount } from "svelte";
+	import { userAddress } from "../../store";
+	import Sidebar from "../../components/Sidebar.svelte";
+	import Topbar from "../../components/Topbar.svelte";
+	import Footer from "../../components/Footer.svelte";
 	const handleBuy = () => {
 		let userAddr;
 		userAddress.subscribe((v) => {
 			userAddr = v;
 			let transak = new transakSDK({
 				// apiKey: '4ad5495c-4b66-4360-aec3-f229b63e4aca', // (Required)
-				apiKey: '0f4beee9-e541-442c-b6bc-bc41a442dfc3', // (Required)
-				environment: 'PRODUCTION', // (Required)
+				apiKey: "0f4beee9-e541-442c-b6bc-bc41a442dfc3", // (Required)
+				environment: "PRODUCTION", // (Required)
 				// .....
 				// For the full list of customisation options check the link above
-				defaultCryptoCurrency: 'MATIC',
-				defaultNetwork: 'polygon,',
-				countryCode: 'IN',
-				fiatCurrency: 'INR',
+				defaultCryptoCurrency: "MATIC",
+				defaultNetwork: "polygon,",
+				countryCode: "IN",
+				fiatCurrency: "INR",
 				// defaultFiatAmount: 2000,
 				// fiatAmount: 2000,
-				themeColor: '1f2937',
-				widgetHeight: '625px',
-				widgetWidth: '500px',
+				themeColor: "1f2937",
+				widgetHeight: "625px",
+				widgetWidth: "500px",
 				walletAddress: userAddr,
-				disableWalletAddressForm: true
+				disableWalletAddressForm: true,
 				// redirectURL:''
 			});
 
@@ -52,9 +52,13 @@
 <div class="bg-gray-800 h-screen overflow-y-hidden">
 	<Sidebar />
 	<Topbar />
-	<div class="ml-16 pt-16 overflow-hidden flex justify-center">
-		<div class="py-12 px-4 bg-gray-900 rounded-md flex flex-col items-center shadow-xl">
-			<h1 class="mt-6 mb-4 text-gray-100 font-bold text-3xl text-center">Buy the native coin</h1>
+	<div class="ml-16 pt-16 pb-12 overflow-hidden flex justify-center">
+		<div
+			class="py-12 px-4 bg-gray-900 rounded-md flex flex-col items-center shadow-xl"
+		>
+			<h1 class="mt-6 mb-4 text-gray-100 font-bold text-3xl text-center">
+				Buy the native coin
+			</h1>
 			<img
 				class="w-1/3"
 				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/polygon/polygon-original.svg"
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,15 +1,15 @@
 <script>
-	let title = 'Kryptonit | Portfolio';
-	import { onMount } from 'svelte';
-	import { redirect } from '@sveltejs/kit';
-	import AllPortfolioChart from '../../components/charts/AllPortfolioChart.svelte';
-	import DonutChart from '../../components/charts/DonutChart.svelte';
-	import PortfolioTokenTable from '../../components/PortfolioTokenTable.svelte';
-	import Sidebar from '../../components/Sidebar.svelte';
-	import Topbar from '../../components/Topbar.svelte';
-	import { userAddress } from '../../store.js';
-	import { options } from '../../utils/config.js';
-	import Footer from '../../components/Footer.svelte';
+	let title = "Kryptonit | Portfolio";
+	import { onMount } from "svelte";
+	import { redirect } from "@sveltejs/kit";
+	import AllPortfolioChart from "../../components/charts/AllPortfolioChart.svelte";
+	import DonutChart from "../../components/charts/DonutChart.svelte";
+	import PortfolioTokenTable from "../../components/PortfolioTokenTable.svelte";
+	import Sidebar from "../../components/Sidebar.svelte";
+	import Topbar from "../../components/Topbar.svelte";
+	import { userAddress } from "../../store.js";
+	import { options } from "../../utils/config.js";
+	import Footer from "../../components/Footer.svelte";
 	let userAddr;
 	let tokenPortfolio;
 	let datas;
@@ -20,16 +20,16 @@
 		async function fetchPortfolio() {
 			let response = await fetch(
 				`https://deep-index.moralis.io/api/v2/${userAddr}/erc20?chain=polygon`,
-				options
+				options,
 			);
 			datas = await response.json();
 			let coinResponse = await fetch(
-				`https://account.metafi.codefi.network/accounts/${userAddr}?chainId=137&includePrices=true`
+				`https://account.metafi.codefi.network/accounts/${userAddr}?chainId=137&includePrices=true`,
 			);
 
 			let coinres = await coinResponse.json();
 			let usdRes = await fetch(
-				'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=INR&api_key=b8e3706ffe847a27b8a0a4060a6df2e2df496fdf63575aadced16daca5dd48ec'
+				"https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=INR&api_key=b8e3706ffe847a27b8a0a4060a6df2e2df496fdf63575aadced16daca5dd48ec",
 			);
 			let usdJson = await usdRes.json();
 			usdValue = usdJson.INR;
@@ -47,15 +47,16 @@
 				value: {
 					price: coinres.nativeBalance.value.price,
 					marketValue: coinres.nativeBalance.value.marketValue,
-					currency: 'usd',
-					pricePercentChange1d: coinres.nativeBalance.value.pricePercentChange1d
-				}
+					currency: "usd",
+					pricePercentChange1d:
+						coinres.nativeBalance.value.pricePercentChange1d,
+				},
 			};
 
 			tokenPortfolio.push(maticObj);
 			for (let res of coinres.tokenBalances) {
 				if (res.value) {
-					if (res.hasOwnProperty('coingeckoId')) {
+					if (res.hasOwnProperty("coingeckoId")) {
 						tokenPortfolio.push(res);
 						tknCount.push(res.balance);
 						portfolioValue += res.value.marketValue;
@@ -71,7 +72,15 @@
 			coingeckoIds.push(coinres.nativeBalance.coingeckoId); //
 			labels.push(coinres.nativeBalance.name); //
 			dataSets.push(coinres.nativeBalance.value.marketValue); //
-			return [tokenPortfolio, labels, dataSets, coingeckoIds, tknCount, portfolioValue, usdValue];
+			return [
+				tokenPortfolio,
+				labels,
+				dataSets,
+				coingeckoIds,
+				tknCount,
+				portfolioValue,
+				usdValue,
+			];
 		}
 	});
 </script>
@@ -80,14 +89,16 @@
 	<title>{title}</title>
 </svelte:head>
 
-<div class="bg-gray-800 pb-20">
+<div class="bg-gray-800">
 	<Topbar />
 	<Sidebar />
-	<div class="ml-16 px-16">
+	<div class="ml-16 px-16 pb-12">
 		<div>
 			<div class="flex justify-between items-center mb-8 mt-6">
 				<h1 class="text-4xl font-extrabold text-slate-100">Portfolio</h1>
-				<div class="inline-block rounded-full border-2 border-slate-400 py-3 px-1">
+				<div
+					class="inline-block rounded-full border-2 border-slate-400 py-3 px-1"
+				>
 					<a
 						class="hover:bg-slate-600 font-medium text-slate-100 py-2 px-4 mx-2 rounded-full transition-all"
 						href="/portfolio">Token</a
@@ -101,7 +112,9 @@
 		</div>
 		{#await tokenPortfolio}
 			<div class="h-[74vh] flex justify-center items-center">
-				<p class="text-4xl font-bold mb-40 text-slate-100 animate-pulse">Loading...</p>
+				<p class="text-4xl font-bold mb-40 text-slate-100 animate-pulse">
+					Loading...
+				</p>
 			</div>
 		{:then tokenPortfolio}
 			{#if tokenPortfolio?.length}
